071fef0f20bff64f06f16e69e22fe61e2ca1c0dc
KristenBelkadiEpsi <kristen.belkadi.etudiant@gmail.com>
2023-06-23 15:29:26 +0200

    test prettier

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -3,6 +3,6 @@ var User = function (name) {
   this.name = name;
 };describe('Checking if the use is created correctly', function () {it('should create the user with the correct name', function () {debugger;
     var tom = new User('tom');should(tom.name).be.equal('tom');
-  });
+});
 });
 ""
