c2b554212ea0819857d090a8c70db65554af49f9
KristenBelkadiEpsi <kristen.belkadi.etudiant@gmail.com>
2023-06-23 15:17:41 +0200

    retest prettier

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -2,7 +2,6 @@ var should = require('should');
 var User = function (name) {
   this.name = name;
 };describe('Checking if the use is created correctly', function () {it('should create the user with the correct name', function () {debugger;
-    var tom = new User('tom');
-    should(tom.name).be.equal('tom');
+    var tom = new User('tom');should(tom.name).be.equal('tom');
   });
 });
